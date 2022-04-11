47241fe9b9fc54cb690022301d3aa11c8f6dcbb7
timdoan22 <timdoan01@gmail.com>
2022-04-11 03:21:25 -0400

    Adding get random cocktail feature

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-document.querySelector('button').addEventListener('click', getDrink)
+document.getElementById('search-cocktail').addEventListener('click', getDrink)
+document.getElementById('random-cocktail').addEventListener('click', randomDrink)
+
 
 function getDrink() {
     const drink = document.querySelector('input').value
@@ -17,5 +19,20 @@ function getDrink() {
         console.log(`error ${err}`)
     });
 }
+
+function randomDrink() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.querySelector('h2').innerText = data.drinks[0].strDrink
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        document.querySelector('h3').innerText = data.drinks[0].strInstructions
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
     
-// console.log(data.message)
+
+www.thecocktaildb.com/api/json/v1/1/random.php
